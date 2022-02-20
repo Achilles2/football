654fb613eb38b72a690aa9289240027e8ece791b
user <u>
2022-02-20 18:52:41 +0200

    Create function: response from json

--- utils.py
+++ utils.py
@@ -1,8 +1,20 @@
+from json import loads
+
+
 def round_num(round_str: str):
     s = round_str.lstrip("Regular Season - ")
     return int(s)
+
 
 def round_in_range(round_number):
     if 6 < int(round_number) < 33:
         return True
     return False
+
+
+def response_from_json(file_name):
+    with open(file_name) as f:
+        lines = f.readlines()
+        as_str = ''.join(lines)
+        as_dict = loads(as_str)
+        return as_dict['response']
